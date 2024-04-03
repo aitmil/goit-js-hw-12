ed5b4394ee8499ab1fc4946f7a8e475a463b2a08
aitmil <120422761+aitmil@users.noreply.github.com>
2024-04-03 21:31:34 +0300

    Render code pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,18 +1,14 @@
-export function getImage(query) {
-  const BASE_URL = 'https://pixabay.com/api/';
-  const API_KEY = '42982989-b171e2f7a41a03eb4f5866708';
-  const params = new URLSearchParams({
-    key: API_KEY,
+import axios from 'axios';
+
+export async function getImage(query) {
+  axios.defaults.baseURL = 'https://pixabay.com/api/';
+  const params = {
+    key: '42982989-b171e2f7a41a03eb4f5866708',
     q: query,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
-  });
-  const url = `${BASE_URL}?${params}`;
-  return fetch(url).then(res => {
-    if (!res.ok) {
-      throw new Error(res.status);
-    }
-    return res.json();
-  });
+  };
+  const res = await axios.get('', { params });
+  return res.data;
 }
